33c7b8f5dc37ee04fb24bd107792a5c1ab48d7c2
Matti Tanskanen <mattitapanitanskanen@gmail.com>
2020-11-05 14:39:34 +0100

    1.4: course information step4

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -22,31 +22,31 @@ const Content = (props) => {
 }
 const Total = (props) => {
     return (
-        <p>Number of exercises {props.numberOfExercises}</p>
+        <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
     )
 }
 const App = () => {
     const course = 'Half stack application development'
-    const part1 = {
-        name: 'Fundamentals of React',
-        exercises: 10
-    }
-    const part2 = {
-        name: 'Using props to pass data',
-        exercises: 7
-    }
-    const part3 = {
-        name: 'State of a component',
-        exercises: 14
-    }
-    const parts = [];
-    parts.push(part1, part2, part3)
+    const parts = [
+        {
+            name: 'Fundamentals of React',
+            exercises: 10
+        },
+        {
+            name: 'Using props to pass data',
+            exercises: 7
+        },
+        {
+            name: 'State of a component',
+            exercises: 14
+        }
+    ];
 
     return (
         <div>
             <Header course={course} />
             <Content parts={parts}/>
-            <Total numberOfExercises={part1.exercises + part2.exercises + part3.exercises} />
+            <Total parts={parts} />
         </div>
     )
 }
