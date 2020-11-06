18199d43476b3411faa3094be6126d1677122b34
Matti Tanskanen <mattitapanitanskanen@gmail.com>
2020-11-06 17:10:10 +0100

    2.2: Course information step7

--- part2/courseinfo/src/components/content.js
+++ part2/courseinfo/src/components/content.js
@@ -2,11 +2,14 @@ import React from 'react';
 import Part from './part';
 
 const Content = ({content}) => {
+    const exercisesInParts = content.map(part => part.exercises)
+    const totalNumberOfExercises = exercisesInParts.reduce((total, exercisesInParts) => total + exercisesInParts)
     return (
         <div>
             {content.map(part =>
                 <Part name={part.name} exercises={part.exercises} key={part.id} />
             )}
+            <strong>total of {totalNumberOfExercises} exercises</strong>
         </div>
     )
 }
